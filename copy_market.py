16218fa3463146106418dbd1c2c73b26184e9261
Eugeny1978 <luchnik78@mail.ru>
2024-02-05 15:43:46 +0300

    Modify Files

--- copy_market.py
+++ copy_market.py
@@ -10,6 +10,7 @@ SYMBOLS_liquid = ('ATOM/USDT', 'BTC/USDT', 'ETH/USDT', 'LINK/USDT', 'TRX/USDT')
 SYMBOLS_shit = ('DEL/USDT')
 PATRON_liquid = 'Constantin_ByBit'
 PATRON_shit = 'Constantin_Mexc'
+DELTA_MIN = 60 # Интервал вглуь которого проверяется наличие новых Сделок
 
 connections = {
     'Binance': ccxt.binance,
@@ -22,7 +23,10 @@ connections = {
 
 def connect_exchange(account):
     try:
-        connection = connections[account['exchange']]()
+        connection: ccxt.Exchange = connections[account['exchange']]() # {'options': {'defaultType': 'spot'}}
+        connection.load_markets()
+        # connection.options['defaultType'] = 'spot'
+        # connection.enableRateLimit = True # по умолчанию уже стоит
         connection.apiKey = account['apiKey']
         connection.secret = account['secret']
         connection.password = account['password']
@@ -30,6 +34,7 @@ def connect_exchange(account):
     except:
         print(f"НЕ Удалось подключиться. Проверьте API Ключи | {account['name']} | {account['exchange']}")
         return None
+
 
 class ClientData:
     def __init__(self, trade: str): # 'Liquid_coins', 'Shit_coins'
@@ -65,7 +70,7 @@ class Trader:
 
     def __init__(self, client: sq.Row):
         self.client = client
-        self.connection = connect_exchange(client)
+        self.connection: ccxt.Exchange = connect_exchange(client)
         self.balance =  self.get_balance()
 
     def get_balance(self):
@@ -85,10 +90,18 @@ class Trader:
 
     def sell_market(self, symbol, amount):
         try:
-            self.connection.create_market_sell_order(symbol=symbol, amount=amount)
-            print(f"ПРОДАНО по Рынку: {symbol} - Объем: {amount} | {self.client['name']} | {self.client['exchange']}")
+            trade = self.connection.create_market_sell_order(symbol=symbol, amount=amount)
+            print(f"{self.client['name']} | {self.client['exchange']} | ПРОДАНО по Рынку: {symbol}, Объем: {trade['amount']}, Цена: {trade['price']}")
         except:
-            print(f"НЕ Удалось Продать по Рынку: {symbol} - Объем: {amount} | {self.client['name']} | {self.client['exchange']}")
+            print(f"{self.client['name']} | {self.client['exchange']} | НЕ Удалось Продать по Рынку: {symbol}, Объем: {amount}")
+
+    def buy_market(self, symbol, amount, price):
+        try:
+            trade = self.connection.create_market_order(symbol=symbol, amount=amount, side='buy', price=price)
+            print(f"{self.client['name']} | {self.client['exchange']} | КУПЛЕНО по Рынку: {symbol}, Объем: {trade['amount']}, Цена: {trade['price']}")
+        except:
+            print(f"{self.client['name']} | {self.client['exchange']} | НЕ Удалось Купить по Рынку: {symbol}, Объем: {amount}")
+
 
 class PatronData:
     name = 'Constantin_ByBit'
@@ -97,7 +110,7 @@ class PatronData:
     def __init__(self, trade: str): # 'Liquid_coins', 'Shit_coins'
         self.name, self.symbols, self.db_table = self.get_constantes(trade)
         self.patron  = self.get_patron()
-        self.connection = connect_exchange(self.patron)
+        self.connection: ccxt.Exchange = connect_exchange(self.patron)
 
     def get_constantes(self, trade: str):
         match trade:
@@ -111,7 +124,6 @@ class PatronData:
                 db_table = 'Patrons_Mexc'
         return name, symbols, db_table
 
-
     def get_patron(self):
         query = (f"""
          SELECT name, exchange, apiKey, secret, password 
@@ -124,38 +136,88 @@ class PatronData:
             curs.execute(query)
             return curs.fetchone()
 
-    def get_trades(self):
-        start_dt = datetime.now() - timedelta(hours=240)
-        start_stamp = mktime(start_dt.timetuple())
-        my_trades = {}
+    def not_id_trade_db(self, id):
+        query = (f"""SELECT * FROM MarketTrades WHERE id IS '{id}'""")
+        with sq.connect(DATABASE) as connect:
+            curs = connect.cursor()
+            curs.execute(query)
+            responce = curs.fetchone()
+            not_trade = False if responce else True
+            return not_trade
+
+    def record_id_trade(self, symbol:str, id:str):
+        with sq.connect(DATABASE) as connect:
+            curs = connect.cursor()
+            curs.execute(f"""INSERT INTO MarketTrades (symbol, id) VALUES ('{symbol}', '{id}')""")
+
+    def get_new_trades(self):
+        start_dt = str(datetime.now() - timedelta(minutes=DELTA_MIN))
+        # start_stamp = mktime(start_dt.timetuple()) # Нужно * 1000 чб получить время по которому сравниваем. не стоку а дату
+        start_stamp = self.connection.parse8601(start_dt) # встроенный метод
+        new_trades = []
         for symbol in self.symbols:
-            symbol_bybit = symbol + ':USDT' # именно у ByBita в данн запросе символы необходимо в таком виде подавать
             try:
-                my_trades[symbol] = self.connection.fetch_my_trades() # symbol=symbol_bybit, since=start_stamp
-                # print(f"ПРОДАНО по Рынку: {symbol} - Объем: {amount} | {client['name']} | {client['exchange']}")
+                last_trades = self.connection.fetch_my_trades(symbol=symbol, since=start_stamp)
+                if len(last_trades):
+                    for trade in last_trades:
+                        if trade['type'] == 'market':
+                            if self.not_id_trade_db(trade['id']):
+                                new_trades.append({'symbol': symbol,
+                                                  'id': trade['id'],
+                                                  'side': trade['side'],
+                                                  'amount': trade['amount'],
+                                                  'cost': trade['cost']})
+                                self.record_id_trade(symbol, trade['id'])
+                    if len(new_trades):
+                        print(f"За последние {DELTA_MIN} мин. БЫЛИ Сделки ПО РЫНКУ:")
             except:
-                pass
-                # print(f"НЕ Удалось Продать по Рынку: {symbol} - Объем: {amount} | {client['name']} | {client['exchange']}")
-        return my_trades # trades
+                print(f"По каким-то причинам Запрос не был Обработан Биржей")
+        return new_trades
+
 
 
 if __name__ == '__main__':
 
     ## БЛОК Формирования КАКОЙ COIN НЕОБХОДИМО продать по рынку
     patron = PatronData('Liquid_coins')
-    my_trades = patron.get_trades() # trades,
-    # print(patron.__dict__)
+    new_deals = patron.get_new_trades()
+    print(*new_deals, sep='\n')
 
-    # orders = {}
-    # for symbol in patron.symbols:
-    #     orders[symbol] = patron.connection.fetch_open_orders(symbol)
-    # orders = patron.connection.fetch_open_orders()
-    # print(json.dumps(orders), sep='\n')
+    # Клиенты
+    clients = ClientData(trade='Liquid_coins').clients
 
-    # exchange.has['fetchMyTrades']
-    print(json.dumps(my_trades))
+    # Обход Клиентов
+    for client in clients:
+
+        trader = Trader(client) # Подготовка к торговле
+        if not trader.connection:
+            continue
+
+        # Обход всех новых сделок
+        for deal in new_deals:
+            symbol = deal['symbol']
+            coin = symbol.split('/')[0]
+            price_usdt = trader.connection.fetch_ticker(symbol)['last'] # нужно ли каждому узнеавать или один раз
+            table = pd.DataFrame(columns=('client', 'exchange', 'rate', 'amount_coin', 'cost_usdt')) # необх только для распечатки таблицы
+            match deal['side']:
+                case 'sell': # Продажа ПО РЫНКУ
+                    amount_coin = trader.get_amount_coin(coin)
+                    cost_usdt = round(price_usdt * amount_coin, 2)
+                    table.loc[len(table)] = (client['name'], client['exchange'], client['rate'], amount_coin, cost_usdt)
+                    if cost_usdt > 10.1:
+                        trader.sell_market(symbol=symbol, amount=amount_coin)
+                case 'buy': # Покупка ПО РЫНКУ
+                    trader.buy_market(symbol=symbol, amount=(deal['amount'] * trader.client['rate']), price=price_usdt)
+            print(coin, table, sep='\n')
 
 
+
+
+
+
+
+
+# ЧЕРНОВИК
     ## БЛОК Копирование Сделок По РЫНКУ. НЕобходимо в предыдущем Блоке сформировать coin (возможно список coins)
     # coin = 'LINK'
     # symbol = f"{coin}/USDT"
